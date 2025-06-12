b5279567882e3e7baf8f067a2a3a7da83a0d2d84
isadorapaulini <169076010+isadorapaulini@users.noreply.github.com>
2025-06-12 08:31:16 -0300

    Add files via upload

---
+++ SENHA SEGURA/script.js
@@ -0,0 +1,35 @@
+const numeroSenha=document.querySelector(".parametro-senha__texto")
+let tamanhoSenha=12
+numeroSenha.textContent=tamanhoSenha
+const botoes=document.querySelectorAll(".parametro-senha__botao")
+function diminuiTamanho(){
+    if(tamanhoSenha>1){
+        tamanhoSenha --
+    }
+numeroSenha.textContent=tamanhoSenha
+// geraSenha()
+
+}
+botoes[0].onclick=diminuiTamanho
+function aumentaTamanho(){
+    if(tamanhoSenha<20){
+        tamanhoSenha ++
+    }
+    numeroSenha.textContent=tamanhoSenha
+    // geraSenha()
+}
+botoes[1].onclick=aumentaTamanho
+
+const campoSenha = document.querySelector("#campo-senha")
+const letrasMaiusculas='ABCDEFGHJIKLMNOPQRSTUVWXYZ'
+campoSenha.value = 'aaaaa'
+// function geraSenha(){
+//     let senha=''
+//     for(let i=0; i<tamanhoSenha; i++){
+
+//     let numeroAleatorio=Math.random()*letrasMaiusculas.length
+//     numeroAleatorio=Math.floor(numeroAleatorio);
+//   senha=senha+ letrasMaiusculas[numeroAleatorio]
+//     }
+//   campoSenha.value=senha
+// }
